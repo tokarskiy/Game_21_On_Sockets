5820d8a1a99c5e26631b371839a2851a41788fd8
Andriy Tokarskiy <tokarskiy@users.noreply.github.com>
2016-06-15 23:52:59 +0300

    Optimized

--- Sockets/ClientFunctions.cs
+++ Sockets/ClientFunctions.cs
@@ -1,78 +1,78 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 
-namespace Sockets
+namespace SocketsLab
 {
-    static class Client
-    {
-        static int score;
-        static Random random;
-        public const int MAX_RANDOM = 10;
-        public static void Start(int port)
-        {
-            random = new Random(DateTime.Now.Millisecond);
-            score = random.Next(MAX_RANDOM);
-            byte[] bytes = new byte[1024];
+	public static class Client
+	{
+		static int score;
+		static Random random;
+		public const int MAX_RANDOM = 10;
+		public static void Start(int port)
+		{
+			random = new Random(DateTime.Now.Millisecond);
+			score = random.Next(MAX_RANDOM);
+			byte[] bytes = new byte[1024];
 
-            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = ipHost.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
-            Console.WriteLine("Количество очков: {0, 2}.", score);
-            while (true)
-            {
-                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                sender.Connect(ipEndPoint);
-                
-                // Игрок берет карту
-                Console.Write("Вы берёте карту? (y/n): ");
-                string message = Console.ReadLine();
-                if (message == "y")
-                {
-                    score += random.Next(MAX_RANDOM);
-                }
+			IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+			IPAddress ipAddress = ipHost.AddressList[0];
+			IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+			Console.WriteLine("Количество очков: {0, 2}.", score);
+			Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			sender.Connect(ipEndPoint);
+			while (true)
+			{
+				// Игрок берет карту
+				Console.Write("Вы берёте карту? (y/n): ");
+				string message = Console.ReadLine();
+				if (message == "y")
+				{
+					score += random.Next(MAX_RANDOM);
+				}
 
-                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+				byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
-                int bytesSend = sender.Send(messageBytes);
-                Console.WriteLine("Количество очков: {0, 2}.", score);
-                // Противник берет карту
-                Console.WriteLine("Ожидание хода противника");
-                int bytesReceive = sender.Receive(bytes);
-                string messageReceive = Encoding.UTF8.GetString(bytes, 0, bytesReceive);
-                Console.WriteLine(messageReceive == "y" ? "Противник взял карту" : "Противник не взял карту");
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
+				sender.Send(messageBytes);
+				Console.WriteLine("Количество очков: {0, 2}.", score);
+				// Противник берет карту
+				Console.WriteLine("Ожидание хода противника");
+				int bytesReceive = sender.Receive(bytes);
+				string messageReceive = Encoding.UTF8.GetString(bytes, 0, bytesReceive);
+				Console.WriteLine(messageReceive == "y" ? "Противник взял карту" : "Противник не взял карту");
 
-                if (messageReceive == "n" && message == "n")
-                {
-                    CompareScores(ipHost, ipAddress, ipEndPoint);
-                    return;
-                }
-            }
-        }
 
-        public static void CompareScores(IPHostEntry ipHost, IPAddress ipAddress, IPEndPoint ipEndPoint)
-        {
-            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            sender.Connect(ipEndPoint);
+				if (messageReceive == "n" && message == "n")
+				{
+					sender.Shutdown(SocketShutdown.Both);
+					sender.Close();
+					CompareScores(ipHost, ipAddress, ipEndPoint);
+					return;
+				}
+			}
+		}
 
-            // Отправить свой счет
-            byte[] messageBytes = Encoding.UTF8.GetBytes(score.ToString());
-            int bytesSend = sender.Send(messageBytes);
+		public static void CompareScores(IPHostEntry ipHost, IPAddress ipAddress, IPEndPoint ipEndPoint)
+		{
+			Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			sender.Connect(ipEndPoint);
 
-            // Получить результат от сервера
-            byte[] resultBytes = new byte[1024];
-            int bytesReceive = sender.Receive(resultBytes);
-            string result = Encoding.UTF8.GetString(resultBytes, 0, bytesReceive);
-            Console.WriteLine(result);
+			// Отправить свой счет
+			byte[] messageBytes = Encoding.UTF8.GetBytes(score.ToString());
+			sender.Send(messageBytes);
 
-            sender.Shutdown(SocketShutdown.Both);
-            sender.Close();
-        }
-    }
+			// Получить результат от сервера
+			byte[] resultBytes = new byte[1024];
+			int bytesReceive = sender.Receive(resultBytes);
+			string result = Encoding.UTF8.GetString(resultBytes, 0, bytesReceive);
+			Console.WriteLine(result);
+
+			sender.Shutdown(SocketShutdown.Both);
+			sender.Close();
+		}
+	}
 }
+
